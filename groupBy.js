2d8c603e56cd15ed1e6abaa07d5c9da1c573e31d
Geetanjali K <45900266+geetanjalikore@users.noreply.github.com>
2022-04-06 09:36:41 +0530

    Changed approach of grouping the elements.
    Extracted a groupBy function.
    Modified groupElements function.

--- groupBy.js
+++ groupBy.js
@@ -1,6 +1,14 @@
 const areArraysEqual = function (arr1, arr2) {
-  for (let index = 0; index < Math.max(arr1.length, arr2.length); index++) {
-    if (arr1[index] !== arr2[index]) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let index = 0; index < arr2.length; index++) {
+    if (!areEqual(arr1[index], arr2[index])) {
       return false;
     }
   }
@@ -8,30 +16,33 @@ const areArraysEqual = function (arr1, arr2) {
 };
 
 const areEqual = function (lhs, rhs) {
-  if (Array.isArray(lhs) && Array.isArray(rhs)) {
-    return areArraysEqual(lhs, rhs);
+  return areArraysEqual(lhs, rhs) || (lhs === rhs);
+};
+
+const groupBy = function (element, groupedElements) {
+  const groups = groupedElements.slice(0);
+  for (let index = 0; index < groups.length; index++) {
+    if (areEqual(element, groups[index][0])) {
+      groups[index].push(element);
+      return groups;
+    }
   }
-  return lhs === rhs;
+  groups.push([element]);
+  return groups;
 };
 
 const groupElements = function (batch) {
-  if (batch.length < 1) {
-    return [];
-  }
-  const newArray = [];
-  const group = [];
+  let groups = [];
   for (let index = 0; index < batch.length; index++) {
-    const location = areEqual(batch[0], batch[index]) ? group : newArray;
-    location.push(batch[index]);
+    groups = groupBy(batch[index], groups);
   }
-  const groupedElements = groupElements(newArray);
-  groupedElements.unshift(group);
-  return groupedElements;
+  return groups;
 };
 
+console.log(groupElements([]));
 console.log(groupElements([1]));
 console.log(groupElements([1, 3]));
 console.log(groupElements([1, 2, 1]));
 console.log(groupElements([1, 2, 3, 1, 2, 4]));
 console.log(groupElements([[1, 1], 1, [1, 1], 1]));
-console.log(groupElements([[1, 1], 1, [1, 1], 1]));
+console.log(groupElements([[1, [1, 1]], 1, [1, [1, 1]], 1]));
