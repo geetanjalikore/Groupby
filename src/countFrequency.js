a813b384d8f21110ac4d4a4dfdbae9c900f2069a
Geetanjali K <45900266+geetanjalikore@users.noreply.github.com>
2022-04-06 15:17:11 +0530

    Renamed countFrequencies.js to countFrequency.js
    Changed function names in countFrequency.js

--- src/countFrequency.js
+++ src/countFrequency.js
@@ -1,6 +1,6 @@
 const isEqual = require('./isEqual.js').isEqual;
 
-const countFrequency = function (element, groups) {
+const countOccurence = function (element, groups) {
   for (let index = 0; index < groups.length; index++) {
     if (isEqual(element, groups[index][0])) {
       groups[index][1]++;
@@ -11,12 +11,12 @@ const countFrequency = function (element, groups) {
   return groups;
 };
 
-const countFrequencies = function (batch) {
+const frequencies = function (batch) {
   const groups = [];
   for (let index = 0; index < batch.length; index++) {
-    countFrequency(batch[index], groups);
+    countOccurence(batch[index], groups);
   }
   return groups;
 };
 
-exports.countFrequencies = countFrequencies;
+exports.countFrequencies = frequencies;
