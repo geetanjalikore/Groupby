5dfb8a6debaf1f441a12d47b95689aa6473a5b56
Geetanjali K <45900266+geetanjalikore@users.noreply.github.com>
2022-04-06 13:08:23 +0530

    Added Tests for groupBy in testGroupBy.js

--- testGroupBy.js
+++ testGroupBy.js
@@ -9,6 +9,13 @@ const testGroupElements = function (list, expected, description) {
 
 const groupElementsTests = function () {
   testGroupElements([], [], 'Empty array');
+  testGroupElements([1], [[1]], 'One entity in array');
+  testGroupElements([1, 3], [[1], [3]], 'unique entities in array');
+  testGroupElements([1, 2, 1], [[1, 1], [2]], 'Identical element in array');
+  testGroupElements([1, 2, 3, 1, 2, 4], [[1, 1], [2, 2], [3], [4]], 'Identical elements in array');
+  testGroupElements([[1, 1], 1, [1, 1], 1], [[[1, 1], [1, 1]], [1, 1]], 'Identical arrays');
+  testGroupElements([[1, [1, 1]], 1, [1, [1, 1]], 1], [[[1, [1, 1]], [1, [1, 1]]], [1, 1]], 'Nested Identical arrays');
+
 };
 
 groupElementsTests();
